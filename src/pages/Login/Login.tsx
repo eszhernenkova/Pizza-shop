a1a4018a825850d51e65dcfb6ff201a4a0bd2830
eszhernenkova <zhernenkova.ekaterina.gmail.com>
2025-02-06 22:41:21 +0400

    login form done

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -5,28 +5,59 @@ import Headling from '../../components/Headling/Headling';
 import Input from '../../components/Input/Input';
 
 import styles from './Login.module.scss';
-import { FormEvent } from 'react';
+import { FormEvent, useState } from 'react';
+import axios, { AxiosError } from 'axios';
+import { PREFIX } from '../../helpers/API';
 
+
+export type LoginForm = {
+    email: {
+        value: string;
+    };
+    password: {
+        value: string;
+    };
+}
 
 function Login() {
-	const submit = (e: FormEvent)=> {
+	const [error, setError] = useState<string | null>();
+     
+	const submit = async (e: FormEvent)=> {
 		e.preventDefault();
-		console.log(e);
+		setError(null);
+		const target = e.target as typeof e.target & LoginForm;
+		const { email, password } = target;
+
+		await sendLogin(email.value,password.value);
+	};
+    
+	const sendLogin = async (email: string, password: string) => {
+		try{
+			const { data } = await axios.post(`${PREFIX}/auth/login`, {
+				email,
+				password
+			});
+			console.log(data);
+		} catch(e){
+			if( e instanceof AxiosError){
+				setError(e.response?.data.message);
+			}
+		}
 	};
 
-
 	return (
-		<div className={styles.login} onSubmit={submit} >
+		<div className={styles.login}  >
 			<Headling>Вход</Headling>
-			<form className={styles.form}>
+			{ error && <div className={styles.error}>{error}</div> }
+			<form className={styles.form} onSubmit={submit} >
 				<div className={styles.field}>
 					<label htmlFor="email">Ваш email</label>
-				    <Input id="email" placeholder="email"/>
+				    <Input id="email" name="email" placeholder="email"/>
 				</div>
 
 				<div className={styles.field}>
 					<label htmlFor="password">Ваш пароль</label>
-				    <Input id="password" type="password" placeholder="password"/>
+				    <Input id="password" type="password" name="password" placeholder="password"/>
 				</div>
 				<Button appearence='big'>Вход</Button>
 			</form>
